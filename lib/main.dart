80751bea47f9e9d143ba94e334af7160b7db72a3
Simon Roux <sim.roux1@gmail.com>
2022-05-23 10:34:00 +0200

    slash screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,8 @@ import 'package:device_info_plus/device_info_plus.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Connexion(),
+    home: FirstScreen(),
   ));
-}
-
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  State<Home> createState() => _HomeState();
 }
 
 class Connexion extends StatefulWidget {
@@ -28,16 +21,92 @@ class Connexion extends StatefulWidget {
   State<Connexion> createState() => _ConnexionState();
 }
 
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  TextEditingController textController = TextEditingController();
+  String? _deviceIdMatricule;
+  String? _deviceId2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12.0),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 600,
+              height: 700,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/Splash_screen.PNG"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                      return Colors.red; // Use the component's default.
+                    },
+                  ),
+                ),
+                onPressed: () {
+                  _getId();
+                },
+                child: const Text('Connexion')
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+  Future<String?> _getId() async {
+    var deviceInfo = DeviceInfoPlugin();
+    String? deviceId;
+
+    if (Platform.isIOS) { // import 'dart:io'
+      var iosDeviceInfo = await deviceInfo.iosInfo;
+      deviceId = await iosDeviceInfo.identifierForVendor; // unique ID on iOS
+    } else {
+      var androidDeviceInfo = await deviceInfo.androidInfo;
+      deviceId = await androidDeviceInfo.androidId; // unique ID on Android
+    }
+    setState(() => _deviceId2 = deviceId);
+  }
+}
 
 class _ConnexionState extends State<Connexion> {
+  TextEditingController textController = TextEditingController();
+  String? _deviceIdMatricule;
+  String? _deviceId;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(43, 43, 43, 1),
       resizeToAvoidBottomInset: true,
       appBar: AppBar(
-        title: const Text('Photo de visite'),
+        title: const Text('Bienvenu !'),
         centerTitle: true,
       ),
       body: SingleChildScrollView(
@@ -46,13 +115,31 @@ class _ConnexionState extends State<Connexion> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
+            TextFormField(
+              style: TextStyle(color: Colors.white),
+              controller: textController,
+              decoration: const InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                labelStyle: TextStyle(
+                    color: Colors.white
+                ),
+                labelText: 'Première connexion ? Entrer votre matricule :',
+              ),
+            ),
             ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed))
+                        return Colors.red;
+                      return Colors.red; // Use the component's default.
+                    },
+                  ),
+                ),
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Home()),
-                  );
+                  _getId();
                 },
                 child: const Text('Go !')
             ),
@@ -62,6 +149,57 @@ class _ConnexionState extends State<Connexion> {
       ),
     );
   }
+  Future<List> getData(maReq, maData) async {
+    final url = Uri.parse('http://fdvrbi.000webhostapp.com/SelectTest.php')
+        .replace(queryParameters: {
+      'req': maReq,
+      'data': maData,
+    });
+    http.Response response = await http.get(url);
+    var data = jsonDecode(response.body);
+    return data;
+  }
+
+
+
+  Future<String?> _getId() async {
+    var deviceInfo = DeviceInfoPlugin();
+    String? deviceId;
+
+    if (Platform.isIOS) { // import 'dart:io'
+      var iosDeviceInfo = await deviceInfo.iosInfo;
+      deviceId = await iosDeviceInfo.identifierForVendor; // unique ID on iOS
+    } else {
+      var androidDeviceInfo = await deviceInfo.androidInfo;
+      deviceId = await androidDeviceInfo.androidId; // unique ID on Android
+    }
+    List ListIdPhone = (await getData("SELECT DISTINCT id_tel FROM users", 'id_tel'));
+    setState(() => _deviceId = deviceId);
+
+    if(ListIdPhone.contains(deviceId)) {
+      String matric = (await getData("SELECT DISTINCT matricule FROM users WHERE id_tel='$deviceId'", 'matricule')).first.replaceAll('"', '');
+      setState(() => _deviceIdMatricule = matric);
+      Navigator.push(
+        this.context,
+        MaterialPageRoute(builder: (context) => Home()),
+      );
+    } else {
+      insertUser();
+    }
+  }
+
+  Future insertUser() async {
+    String monMatricule = textController.text;
+    if(monMatricule!='' && monMatricule.length==7) {
+      final url = Uri.parse('http://fdvrbi.000webhostapp.com/insert.php')
+          .replace(queryParameters: {
+        'req': "UPDATE users SET id_tel = '$_deviceId' WHERE matricule='$monMatricule'",
+      });
+      http.Response response = await http.post(url);
+      _getId();
+    }
+  }
+
 }
 
 
@@ -75,9 +213,10 @@ class _HomeState extends State<Home> {
   String errMessage = 'Error Uploading Image';
   String? _selectMagasin;
   String? _selectTheme;
+  String? _deviceIdMatricule;
   String? _deviceId;
   TextEditingController textController = TextEditingController();
-  int Note = 0;
+  int Note = 5;
   @override
   Widget build(BuildContext context) {
 
@@ -109,22 +248,25 @@ class _HomeState extends State<Home> {
                     );
                   }
                   return
-                    DropdownButton<String>(
-                      isExpanded: true,
-                      value: _selectMagasin,
-                      onChanged: (String? magasin) {
-                        setState(() {
-                          _selectMagasin = magasin!;
-                        });
-                      },
-                      style: const TextStyle(color: Colors.blue),
-                      items: listeMagasins.map((String value) {
-                        return
-                          DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value.replaceAll('"', '')),
-                          );
-                      }).toSet().toList(),
+                    Container(
+                      height: 30,
+                      child: DropdownButton<String>(
+                        isExpanded: true,
+                        value: _selectMagasin,
+                        onChanged: (String? magasin) {
+                          setState(() {
+                            _selectMagasin = magasin!;
+                          });
+                        },
+                        style: const TextStyle(color: Colors.blue),
+                        items: listeMagasins.map((String value) {
+                          return
+                            DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value.replaceAll('"', '')),
+                            );
+                        }).toSet().toList(),
+                      )
                     );
                 } else {
                   return const Center(child: CircularProgressIndicator());
@@ -136,8 +278,7 @@ class _HomeState extends State<Home> {
               future: getData('SELECT DISTINCT theme FROM themes', 'theme'),
               builder: (BuildContext context,
                   AsyncSnapshot<dynamic> snapshot) {
-                if (snapshot.connectionState == ConnectionState.done &&
-                    snapshot.hasData && snapshot.data != null) {
+                if (snapshot.connectionState == ConnectionState.done && snapshot.hasData && snapshot.data != null) {
                   List<String> listeThemes = [];
                   for (var i = 0; i < snapshot.data.length; i++) {
                     listeThemes.add(snapshot.data[i].replaceAll("[", "")
@@ -145,23 +286,26 @@ class _HomeState extends State<Home> {
                     );
                   }
                   return
-                    DropdownButton<String>(
-                      isExpanded: true,
-                      value: _selectTheme,
-                      onChanged: (String? theme) {
-                        setState(() {
-                          _selectTheme = theme!;
-                        });
-                      },
-                      style: const TextStyle(color: Colors.blue),
-                      items: listeThemes.map((String value) {
-                        return
-                          DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value.replaceAll('"', '').replaceAll(
-                                "_", " ")),
-                          );
-                      }).toSet().toList(),
+                    Container(
+                      height: 40,
+                      child: DropdownButton<String>(
+                        isExpanded: true,
+                        value: _selectTheme,
+                        onChanged: (String? theme) {
+                          setState(() {
+                            _selectTheme = theme!;
+                          });
+                        },
+                        style: const TextStyle(color: Colors.blue),
+                        items: listeThemes.map((String value) {
+                          return
+                            DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value.replaceAll('"', '').replaceAll(
+                                  "_", " ")),
+                            );
+                        }).toSet().toList(),
+                      )
                     );
                 } else {
                   return const Center(child: CircularProgressIndicator());
@@ -173,7 +317,7 @@ class _HomeState extends State<Home> {
             if(tmpFile != null)
               Container(
                 width: 640,
-                height: 400,
+                height: 350,
                 alignment: Alignment.topCenter,
                 decoration: BoxDecoration(
                   image: DecorationImage(
@@ -184,7 +328,7 @@ class _HomeState extends State<Home> {
             else
               Container(
                 width: 640,
-                height: 400,
+                height: 350,
                 alignment: Alignment.center,
                 child: const Text(
                   'Votre photo',
@@ -235,6 +379,15 @@ class _HomeState extends State<Home> {
               children: [
                 Expanded(
                   child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                              (Set<MaterialState> states) {
+                            if (states.contains(MaterialState.pressed))
+                              return Colors.red;
+                            return Colors.red; // Use the component's default.
+                          },
+                        ),
+                      ),
                       onPressed: () => getImage(source: ImageSource.camera),
                       child: const Text(
                           'Appareil photo', style: TextStyle(fontSize: 18))
@@ -243,6 +396,15 @@ class _HomeState extends State<Home> {
                 const SizedBox(width: 20,),
                 Expanded(
                   child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                              (Set<MaterialState> states) {
+                            if (states.contains(MaterialState.pressed))
+                              return Colors.red;
+                            return Colors.red; // Use the component's default.
+                          },
+                        ),
+                      ),
                       onPressed: () => getImage(source: ImageSource.gallery),
                       child: const Text(
                           'Galerie', style: TextStyle(fontSize: 18))
@@ -254,6 +416,15 @@ class _HomeState extends State<Home> {
               children: [
                 Expanded(
                   child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                              (Set<MaterialState> states) {
+                            if (states.contains(MaterialState.pressed))
+                              return Colors.red;
+                            return Colors.red; // Use the component's default.
+                          },
+                        ),
+                      ),
                       onPressed: () => uploadFile(tmpFile),
                       child: const Text(
                           'Enregistrer', style: TextStyle(fontSize: 18))
@@ -277,15 +448,16 @@ class _HomeState extends State<Home> {
   }
 
   Future insertData(monImage) async {
-    String monMag = _selectMagasin!.substring(1, 6);
+    _getId();
+    String monMag = _selectMagasin!.substring(0, 5);
     String monMotif = _selectTheme!.replaceAll('"', '');
     String monComm = textController.text;
     int maNote = Note;
-    _getId();
-    print("INSERT INTO fiche_visite_autre2 (Code_CM, Motif_Visite, Matricule_creation, Rapport_Visite, IMAGE_1, NOTE_1) VALUES ('$monMag', '$monMotif', '$_deviceId', '$monComm', '$monImage', '$maNote')");
+
+    print(_deviceIdMatricule);
     final url = Uri.parse('http://fdvrbi.000webhostapp.com/insert.php')
         .replace(queryParameters: {
-      'req': "INSERT INTO fiche_visite_autre2 (Code_CM, Motif_Visite, Matricule_creation, Rapport_Visite, IMAGE_1, NOTE_1) VALUES ('$monMag', '$monMotif', '$_deviceId', '$monComm', '$monImage', '$maNote')",
+      'req': "INSERT INTO fiche_visite_autre2 (Code_CM, Motif_Visite, Matricule_creation, Rapport_Visite, IMAGE_1, NOTE_1) VALUES ('$monMag', '$monMotif', '$_deviceIdMatricule', '$monComm', '$monImage', '$maNote')",
     });
     http.Response response = await http.post(url);
   }
@@ -309,6 +481,15 @@ class _HomeState extends State<Home> {
             content: Text(response.toString().replaceAll('"', ' ')),
             actions: [
               ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                          (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.pressed))
+                          return Colors.red;
+                        return Colors.red; // Use the component's default.
+                      },
+                    ),
+                  ),
                   onPressed: () {
                     Navigator.pop(context);
                   },
@@ -335,8 +516,10 @@ class _HomeState extends State<Home> {
       deviceId = await androidDeviceInfo.androidId; // unique ID on Android
     }
     String matric = (await getData("SELECT DISTINCT matricule FROM users WHERE id_tel='$deviceId'", 'matricule')).first.replaceAll('"', '');
-    print("SELECT DISTINCT matricule FROM users WHERE id_tel='$deviceId'");
-    setState(() => _deviceId = matric);
+    print(matric);
+    print(deviceId);
+    setState(() => _deviceIdMatricule = matric);
+    setState(() => _deviceId = deviceId);
   }
 
   Future<List> getData(maReq, maData) async {
