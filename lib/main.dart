b27a91532af2b82f33105e653d82517f2572da31
Simon Roux <sim.roux1@gmail.com>
2022-06-14 14:38:51 +0200

    add data Table from SQL

--- lib/main.dart
+++ lib/main.dart
@@ -90,99 +90,75 @@ class _MesFichesState extends State<MesFiches> {
             future: getData('SELECT * FROM fiche_visite_autre2', '*'),
             builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
               if (snapshot.connectionState == ConnectionState.done && snapshot.hasData && snapshot.data != null) {
-                List<String> listeID = [];
-                List<String> listeCodeCM = [];
-                List<String> listeDate_Visite = [];
-                List<String> listeDuree_visite_en_jours = [];
-                List<String> listeMotif_Visite = [];
-                List<String> listeNOTE_1 = [];
-                List<String> listeRapport_Visite = [];
-                //print(snapshot.data);
+                List<dynamic> _userTransactionList = snapshot.data;
 
-                for (var i = 0; i < 10; i++) {
-                  listeID.add(snapshot.data[i]['ID']);
-                  listeCodeCM.add(snapshot.data[i]['Code_CM']);
-                  listeDate_Visite.add(snapshot.data[i]['Date_Visite']);
-                  listeDuree_visite_en_jours.add(snapshot.data[i]['Duree_visite_en_jours']);
-                  listeMotif_Visite.add(snapshot.data[i]['Motif_Visite']);
-                  listeNOTE_1.add(snapshot.data[i]['NOTE_1']);
-                  listeRapport_Visite.add(snapshot.data[i]['Rapport_Visite']);
-                }
-                final listOfColumns = snapshot.data.map((dynamic value) {
-                  print(value['Code_CM']);
-                }).toSet().toList();
-                return DataTable(
-                  columns: const <DataColumn>[
-                    DataColumn(
-                      label: Text(
-                        'ID',
-                        style: TextStyle(fontStyle: FontStyle.italic),
+                  List<DataRow> rows = [];
+                  for (var i = 0; i < _userTransactionList.length; i++) {
+                    rows.add(DataRow(cells: [
+                      DataCell(
+                        Text(_userTransactionList[i]['ID'].toString()),
                       ),
-                    ),
-                    DataColumn(
-                      label: Text(
-                        'Code CM',
-                        style: TextStyle(fontStyle: FontStyle.italic),
+                      DataCell(
+                        Text(_userTransactionList[i]['Code_CM'].toString()),
                       ),
-                    ),
-                    DataColumn(
-                      label: Text(
-                        'Date visite',
-                        style: TextStyle(fontStyle: FontStyle.italic),
+                      DataCell(
+                        Text(_userTransactionList[i]['Date_Visite'].toString()),
                       ),
-                    ),
-                    DataColumn(
-                      label: Text(
-                        'Durée',
-                        style: TextStyle(fontStyle: FontStyle.italic),
+                      DataCell(
+                        IconButton(
+                          onPressed: () {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Matthew 34 199.55'),
+                              ),
+                            );
+                          },
+                          icon: const Icon(
+                            Icons.email_outlined,
+                            color: Colors.red,
+                          ),
+                        ),
                       ),
-                    ),
-                    DataColumn(
-                      label: Text(
-                        'Motif',
-                        style: TextStyle(fontStyle: FontStyle.italic),
-                      ),
-                    ),
-                    DataColumn(
-                      label: Text(
-                        'Note',
-                        style: TextStyle(fontStyle: FontStyle.italic),
-                      ),
-                    ),
-                    DataColumn(
-                      label: Text(
-                        'Rapport',
-                        style: TextStyle(fontStyle: FontStyle.italic),
-                      ),
-                    ),
-                  ],
-                  rows: <DataRow>[
-//                      for (var i = 0; i < 10; i++) {
-                      DataRow(
-                        cells: <DataCell>[
-                          DataCell(Text(listeID[0].toString())),
-                          DataCell(Text(listeCodeCM[0].toString())),
-                          DataCell(Text(listeDate_Visite[0].toString())),
-                          DataCell(Text(listeDuree_visite_en_jours[0].toString())),
-                          DataCell(Text(listeMotif_Visite[0].toString())),
-                          DataCell(Text(listeNOTE_1[0].toString())),
-                          DataCell(Text(listeRapport_Visite[0].toString())),
+                    ]));
+                  }
+                  return SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      //child: _getData01(_userTransactionList, context)
+                      child: DataTable(
+                        columnSpacing: 30.0,
+                        columns: const <DataColumn>[
+                          DataColumn(
+                            label: Text(
+                              'ID',
+                              style: TextStyle(color: Colors.deepOrange, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Code_CM',
+                              style: TextStyle(color: Colors.deepOrange, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Date',
+                              style: TextStyle(color: Colors.deepOrange, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Afficher',
+                              style: TextStyle(color: Colors.deepOrange, fontWeight: FontWeight.bold),
+                            ),
+                          ),
                         ],
-                      ),
-                      DataRow(
-                        cells: <DataCell>[
-                          DataCell(Text(listeID[1].toString())),
-                          DataCell(Text(listeCodeCM[1].toString())),
-                          DataCell(Text(listeDate_Visite[1].toString())),
-                          DataCell(Text(listeDuree_visite_en_jours[1].toString())),
-                          DataCell(Text(listeMotif_Visite[1].toString())),
-                          DataCell(Text(listeNOTE_1[1].toString())),
-                          DataCell(Text(listeRapport_Visite[1].toString())),
-                        ],
-                      ),
-//                    }
-                  ],
-                );
+                        rows: rows,
+                      ));
+
+
+
+
+
               } else {
                 return const Center(child: CircularProgressIndicator());
               }
