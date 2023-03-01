705d0fc406345540f73195d023a66b23489e17c8
Simon Roux <sim.roux1@gmail.com>
2023-03-01 14:49:58 +0100

    Modif req magasin

--- lib/main.dart
+++ lib/main.dart
@@ -261,7 +261,7 @@ class _HomeState extends State<Home> {
           children: [
             FutureBuilder(
               future: getData(
-                  'SELECT DISTINCT "test" as Code_CM, CONCAT(Code_CM," - ",nom_mag) as Code_CM FROM magasin WHERE Code_CM LIKE "%CM%" AND OuvertFerme = 0 ORDER BY nom_mag',
+                  'SELECT DISTINCT "test" as Code_CM, CONCAT(Code_CM," - ",nom_mag) as Code_CM FROM magasin WHERE OuvertFerme = 0 ORDER BY nom_mag',
                   'Code_CM'),
               builder: (BuildContext context,
                   AsyncSnapshot<dynamic> snapshot) {
